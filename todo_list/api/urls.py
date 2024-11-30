4cb0d55948b6b422f8d2cbd73aac51f6e2f8c00d
Sepehr Sh <sepehrshafiee1379@gmail.com>
2024-11-30 21:11:54 +0330

    add registration apis

--- todo_list/api/urls.py
+++ todo_list/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('todo/', include(('todo_list.todo.urls', 'todo'))),
-    path('auth/', include(('todo_list.authentication.urls', 'auth')))
+    path('auth/', include(('todo_list.authentication.urls', 'auth'))),
+    path('user/', include(('todo_list.users.urls', 'users')))
 ]
