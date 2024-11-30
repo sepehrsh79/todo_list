3c8884a2a95e8cbe751b1d0dc1bc43c53205040c
Sepehr Sh <sepehrshafiee1379@gmail.com>
2024-11-30 20:59:54 +0330

    remove example app

--- todo_list/example/services/example.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import QuerySet
-from todo_list.example.models import Example
-
-
-def create_example(*, title: str) -> QuerySet[Example]:
-    return Example.objects.create(title=title)
