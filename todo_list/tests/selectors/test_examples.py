0469cfeb95fc96ebde7deec1db36ca524f57736b
Sepehr Sh <sepehrshafiee1379@gmail.com>
2024-11-30 21:01:35 +0330

    remove example tests

--- todo_list/tests/selectors/test_examples.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from todo_list.example.selectors.examples import example_list
-
-
-@pytest.mark.django_db
-def test_example_list(user2, user1, example1):
-    a = example_list(user = user1)
-    assert a.first() == example1
-
